384fe109229dac51ee20aa75ef76e9e735da3e3b
Choo Hyun Ho <hyunho4178@gmail.com>
2023-09-16 22:09:11 +0900

    login screen

--- lib/features/authentication/widgets/sign_up_screen.dart
+++ lib/features/authentication/widgets/sign_up_screen.dart
@@ -3,7 +3,6 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/authentication/widgets/auth_button.dart';
-import 'package:tiktok_clone/features/authentication/widgets/email_screen.dart';
 import 'package:tiktok_clone/features/authentication/widgets/login_screen.dart';
 import 'package:tiktok_clone/features/authentication/widgets/username_screen.dart';
 
