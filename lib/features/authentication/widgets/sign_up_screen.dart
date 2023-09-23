a0b503fe660061256d57ed24df0d7991c9963982
Choo Hyun Ho <hyunho4178@gmail.com>
2023-09-24 03:40:40 +0900

    navigator await push() - 사용자가 예를들어 모달에서 무언가를 제출하고 돌아온 화면에서 그것을 반영하여 데이터를 바꿔서 보여주고 싶을 때

--- lib/features/authentication/widgets/sign_up_screen.dart
+++ lib/features/authentication/widgets/sign_up_screen.dart
@@ -12,10 +12,13 @@ import 'package:tiktok_clone/utils.dart';
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
 
-  void _onLoginTap(BuildContext context) {
-    Navigator.of(context).push(MaterialPageRoute(
-      builder: (context) => const LoginScreen(),
-    ));
+  void _onLoginTap(BuildContext context) async {
+    final result = await Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const LoginScreen(),
+      ),
+    );
+    print(result);
   }
 
   void _onEmailTap(BuildContext context) {
