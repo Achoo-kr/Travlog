243d3759e0c59f2e60cbbd0090e98815f8591082
Choo Hyun Ho <hyunho4178@gmail.com>
2023-09-25 00:43:43 +0900

    goRouter - 파라미터를 부여하여 url로 navigate(굉장히 web스러운 방식, 회원가입과 같이 넘어갈수 없도록 하는 곳에선 Navigator 쓰는게 나을듯)

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -8,7 +8,9 @@ import 'package:tiktok_clone/features/users/widget/persistent_tab_bar.dart';
 import 'package:tiktok_clone/features/users/widget/user_info.dart';
 
 class UserProfileScreen extends StatefulWidget {
-  const UserProfileScreen({super.key});
+  final String username;
+
+  const UserProfileScreen({super.key, required this.username});
 
   @override
   State<UserProfileScreen> createState() => _UserProfileScreenState();
@@ -35,7 +37,7 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
             headerSliverBuilder: (context, innerBoxIsScrolled) {
               return [
                 SliverAppBar(
-                  title: const Text('CHOO'),
+                  title: Text(widget.username),
                   actions: [
                     IconButton(
                       onPressed: _onGearPressed,
@@ -58,9 +60,9 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
                             Row(
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: [
-                                const Text(
-                                  "@CHOO",
-                                  style: TextStyle(
+                                Text(
+                                  "@${widget.username}",
+                                  style: const TextStyle(
                                       fontWeight: FontWeight.w600,
                                       fontSize: Sizes.size16),
                                 ),
