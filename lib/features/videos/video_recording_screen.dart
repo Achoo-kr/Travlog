43d76a42f4c30e469f40be2669555aea9045bd4e
Choo Hyun Ho <hyunho4178@gmail.com>
2023-09-26 03:32:24 +0900

    버그 수정 and goRouterWithAnimation

--- lib/features/videos/video_recording_screen.dart
+++ lib/features/videos/video_recording_screen.dart
@@ -14,7 +14,6 @@ class VideoRecordingScreen extends StatefulWidget {
   static const String routeName = "postVideo";
   static const String routeURL = "/upload";
   const VideoRecordingScreen({super.key});
-
   @override
   State<VideoRecordingScreen> createState() => _VideoRecordingScreenState();
 }
@@ -22,20 +21,15 @@ class VideoRecordingScreen extends StatefulWidget {
 class _VideoRecordingScreenState extends State<VideoRecordingScreen>
     with TickerProviderStateMixin, WidgetsBindingObserver {
   bool _hasPermission = false;
-
   bool _isSelfieMode = false;
-
   late final bool _noCamera = kDebugMode && Platform.isIOS;
-
   late final AnimationController _buttonAnimationController =
       AnimationController(
     vsync: this,
     duration: const Duration(milliseconds: 200),
   );
-
   late final Animation<double> _buttonAnimation =
       Tween(begin: 1.0, end: 1.3).animate(_buttonAnimationController);
-
   late final AnimationController _progressAnimationController =
       AnimationController(
     vsync: this,
@@ -43,44 +37,8 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen>
     lowerBound: 0.0,
     upperBound: 1.0,
   );
-
   late FlashMode _flashMode;
-
   late CameraController _cameraController;
-
-  Future<void> initCamera() async {
-    final cameras = await availableCameras();
-
-    if (cameras.isEmpty) {
-      return;
-    }
-    _cameraController = CameraController(
-      cameras[_isSelfieMode ? 1 : 0],
-      ResolutionPreset.ultraHigh,
-      enableAudio: false,
-    );
-    await _cameraController.initialize();
-    await _cameraController.prepareForVideoRecording();
-
-    _flashMode = _cameraController.value.flashMode;
-
-    setState(() {});
-  }
-
-  Future<void> initPermissions() async {
-    final cameraPermission = await Permission.camera.request();
-    final micPermission = await Permission.microphone.request();
-    final cameraDenied =
-        cameraPermission.isDenied || cameraPermission.isPermanentlyDenied;
-    final micDenied =
-        micPermission.isDenied || micPermission.isPermanentlyDenied;
-    if (!cameraDenied && !micDenied) {
-      _hasPermission = true;
-      await initCamera();
-      setState(() {});
-    }
-  }
-
   @override
   void initState() {
     super.initState();
@@ -102,6 +60,58 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen>
     });
   }
 
+  @override
+  void dispose() {
+    _progressAnimationController.dispose();
+    _buttonAnimationController.dispose();
+    if (!_noCamera) {
+      _cameraController.dispose();
+    }
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (_noCamera) return;
+    if (!_hasPermission) return;
+    if (!_cameraController.value.isInitialized) return;
+    if (state == AppLifecycleState.inactive) {
+      _cameraController.dispose();
+    } else if (state == AppLifecycleState.resumed) {
+      initCamera();
+    }
+  }
+
+  Future<void> initCamera() async {
+    final cameras = await availableCameras();
+    if (cameras.isEmpty) {
+      return;
+    }
+    _cameraController = CameraController(
+      cameras[_isSelfieMode ? 1 : 0],
+      ResolutionPreset.ultraHigh,
+      enableAudio: false,
+    );
+    await _cameraController.initialize();
+    await _cameraController.prepareForVideoRecording();
+    _flashMode = _cameraController.value.flashMode;
+    setState(() {});
+  }
+
+  Future<void> initPermissions() async {
+    final cameraPermission = await Permission.camera.request();
+    final micPermission = await Permission.microphone.request();
+    final cameraDenied =
+        cameraPermission.isDenied || cameraPermission.isPermanentlyDenied;
+    final micDenied =
+        micPermission.isDenied || micPermission.isPermanentlyDenied;
+    if (!cameraDenied && !micDenied) {
+      _hasPermission = true;
+      await initCamera();
+      setState(() {});
+    }
+  }
+
   Future<void> _toggleSelfieMode() async {
     _isSelfieMode = !_isSelfieMode;
     await initCamera();
@@ -116,7 +126,6 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen>
 
   Future<void> _starRecording(TapDownDetails _) async {
     if (_cameraController.value.isRecordingVideo) return;
-
     await _cameraController.startVideoRecording();
     _buttonAnimationController.forward();
     _progressAnimationController.forward();
@@ -124,14 +133,10 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen>
 
   Future<void> _stopRecording() async {
     if (!_cameraController.value.isRecordingVideo) return;
-
     _buttonAnimationController.reverse();
     _progressAnimationController.reset();
-
     final video = await _cameraController.stopVideoRecording();
-
     if (!mounted) return;
-
     Navigator.push(
       context,
       MaterialPageRoute(
@@ -148,9 +153,7 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen>
       source: ImageSource.gallery,
     );
     if (video == null) return;
-
     if (!mounted) return;
-
     Navigator.push(
       context,
       MaterialPageRoute(
@@ -160,25 +163,6 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen>
         ),
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    _progressAnimationController.dispose();
-    _buttonAnimationController.dispose();
-    _cameraController.dispose();
-    super.dispose();
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    if (!_hasPermission) return;
-    if (!_cameraController.value.isInitialized) return;
-    if (state == AppLifecycleState.inactive) {
-      _cameraController.dispose();
-    } else if (state == AppLifecycleState.resumed) {
-      initCamera();
-    }
   }
 
   @override
@@ -206,6 +190,20 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen>
                 children: [
                   if (!_noCamera && _cameraController.value.isInitialized)
                     CameraPreview(_cameraController),
+                  Positioned(
+                    top: Sizes.size40,
+                    left: Sizes.size20,
+                    child: Container(
+                      padding: const EdgeInsets.all(Sizes.size12),
+                      decoration: const BoxDecoration(
+                        color: Colors.grey,
+                        shape: BoxShape.circle,
+                      ),
+                      child: const CloseButton(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
                   if (!_noCamera)
                     Positioned(
                       top: Sizes.size32,
