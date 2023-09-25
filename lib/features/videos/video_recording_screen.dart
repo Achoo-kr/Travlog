2c5a85616ad3ecb9e6fd5492a5b8c250c1801548
Choo Hyun Ho <hyunho4178@gmail.com>
2023-09-25 20:37:47 +0900

    initialize CameraController

---
+++ lib/features/videos/video_recording_screen.dart
@@ -0,0 +1,192 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class VideoRecordingScreen extends StatefulWidget {
+  const VideoRecordingScreen({super.key});
+
+  @override
+  State<VideoRecordingScreen> createState() => _VideoRecordingScreenState();
+}
+
+class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
+  bool _hasPermission = false;
+
+  bool _isSelfieMode = false;
+
+  late FlashMode _flashMode;
+
+  late CameraController _cameraController;
+
+  Future<void> initCamera() async {
+    final cameras = await availableCameras();
+
+    if (cameras.isEmpty) {
+      return;
+    }
+
+    _cameraController = CameraController(
+      cameras[_isSelfieMode ? 1 : 0],
+      ResolutionPreset.ultraHigh,
+    );
+
+    await _cameraController.initialize();
+
+    _flashMode = _cameraController.value.flashMode;
+  }
+
+  Future<void> initPermissions() async {
+    final cameraPermission = await Permission.camera.request();
+    final micPermission = await Permission.microphone.request();
+    final cameraDenied =
+        cameraPermission.isDenied || cameraPermission.isPermanentlyDenied;
+    final micDenied =
+        micPermission.isDenied || micPermission.isPermanentlyDenied;
+    if (!cameraDenied && !micDenied) {
+      _hasPermission = true;
+      await initCamera();
+      setState(() {});
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initPermissions();
+  }
+
+  Future<void> _toggleSelfieMode() async {
+    _isSelfieMode = !_isSelfieMode;
+    await initCamera();
+    setState(() {});
+  }
+
+  Future<void> _setFlashMode(FlashMode newFlashMode) async {
+    await _cameraController.setFlashMode(newFlashMode);
+    _flashMode = newFlashMode;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: !_hasPermission || !_cameraController.value.isInitialized
+            ? const Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Initializing...",
+                    style:
+                        TextStyle(color: Colors.white, fontSize: Sizes.size20),
+                  ),
+                  Gaps.v20,
+                  CircularProgressIndicator.adaptive()
+                ],
+              )
+            : Stack(
+                alignment: Alignment.center,
+                children: [
+                  CameraPreview(_cameraController),
+                  Positioned(
+                    top: Sizes.size32,
+                    left: Sizes.size10,
+                    child: Column(
+                      children: [
+                        IconButton(
+                          color: Colors.white,
+                          onPressed: _toggleSelfieMode,
+                          icon: Container(
+                            padding: const EdgeInsets.all(Sizes.size12),
+                            decoration: const BoxDecoration(
+                              color: Colors.grey,
+                              shape: BoxShape.circle,
+                            ),
+                            child: const Icon(
+                              Icons.cameraswitch,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        Gaps.v10,
+                        IconButton(
+                          color: _flashMode == FlashMode.off
+                              ? Colors.amber.shade200
+                              : Colors.white,
+                          onPressed: () => _setFlashMode(FlashMode.off),
+                          icon: Container(
+                            padding: const EdgeInsets.all(Sizes.size12),
+                            decoration: const BoxDecoration(
+                              color: Colors.grey,
+                              shape: BoxShape.circle,
+                            ),
+                            child: const Icon(
+                              Icons.flash_off_rounded,
+                            ),
+                          ),
+                        ),
+                        Gaps.v10,
+                        IconButton(
+                          color: _flashMode == FlashMode.always
+                              ? Colors.amber.shade200
+                              : Colors.white,
+                          onPressed: () => _setFlashMode(FlashMode.always),
+                          icon: Container(
+                            padding: const EdgeInsets.all(Sizes.size12),
+                            decoration: const BoxDecoration(
+                              color: Colors.grey,
+                              shape: BoxShape.circle,
+                            ),
+                            child: const Icon(
+                              Icons.flash_on_rounded,
+                            ),
+                          ),
+                        ),
+                        Gaps.v10,
+                        IconButton(
+                          color: _flashMode == FlashMode.auto
+                              ? Colors.amber.shade200
+                              : Colors.white,
+                          onPressed: () => _setFlashMode(FlashMode.auto),
+                          icon: Container(
+                            padding: const EdgeInsets.all(Sizes.size12),
+                            decoration: const BoxDecoration(
+                              color: Colors.grey,
+                              shape: BoxShape.circle,
+                            ),
+                            child: const Icon(
+                              Icons.flash_auto_rounded,
+                            ),
+                          ),
+                        ),
+                        Gaps.v10,
+                        IconButton(
+                          color: _flashMode == FlashMode.torch
+                              ? Colors.amber.shade200
+                              : Colors.white,
+                          onPressed: () => _setFlashMode(FlashMode.torch),
+                          icon: Container(
+                            padding: const EdgeInsets.all(Sizes.size12),
+                            decoration: const BoxDecoration(
+                              color: Colors.grey,
+                              shape: BoxShape.circle,
+                            ),
+                            child: const Icon(
+                              Icons.flashlight_on_rounded,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+      ),
+    );
+  }
+}
