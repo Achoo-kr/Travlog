21d8657fa52d809d4a086e04fb87e5835d3d913a
Choo Hyun Ho <hyunho4178@gmail.com>
2023-09-27 02:43:43 +0900

    ValueNotifier

--- lib/common/widgets/main_navigation/widgets/video_config/video_config.dart
+++ lib/common/widgets/main_navigation/widgets/video_config/video_config.dart
@@ -1,21 +1,3 @@
 import 'package:flutter/widgets.dart';
 
-class VideoConfig extends ChangeNotifier {
-  bool autoMute = false;
-
-  void toggleAutoMute() {
-    autoMute = !autoMute;
-    notifyListeners();
-  }
-}
-
-final videoConfig = VideoConfig();
-
-
-//bool _autoMute = videoConfig.autoMute;
-//  videoConfig.addListener(() {
-//    setState(() {
-//      _autoMute = videoConfig.autoMute;
-//    });
-//  });
-//}
+final videoConfig = ValueNotifier(false);
