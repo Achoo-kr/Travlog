b12152481cf6f55d89594d59dd192977761b31af
Choo Hyun Ho <hyunho4178@gmail.com>
2023-10-12 16:44:23 +0900

    Revert "Revert "Revert "Revert "video post - video fetch""""
    This reverts commit 52c5705291eb76813f4457143c6b6698975b5ad4.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,9 +8,7 @@
  */
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-
 admin.initializeApp();
-
 export const onVideoCreated = functions.firestore
   .document("videos/{videoId}")
   .onCreate(async (snapshot, context) => {
@@ -28,9 +26,21 @@ export const onVideoCreated = functions.firestore
       `/tmp/${snapshot.id}.jpg`,
     ]);
     const storage = admin.storage();
-    await storage.bucket().upload(`/tmp/${snapshot.id}.jpg`, {
+    const [file, _] = await storage.bucket().upload(`/tmp/${snapshot.id}.jpg`, {
       destination: `thumbnails/${snapshot.id}.jpg`,
     });
+    await file.makePublic();
+    await snapshot.ref.update({ thumbnailUrl: file.publicUrl() });
+    const db = admin.firestore();
+    await db
+      .collection("users")
+      .doc(video.creatorUid)
+      .collection("videos")
+      .doc(snapshot.id)
+      .set({
+        thumbnailUrl: file.publicUrl(),
+        videoId: snapshot.id,
+      });
   });
 
 // Start writing functions
